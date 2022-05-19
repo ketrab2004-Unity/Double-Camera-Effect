b1f729b23a998705cd83ddd393c7567f3c4da014
Bartek O <bartek@fos.nl>
2022-05-19 14:35:22 +0200

    Init commit
    september 2020

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraFocus : MonoBehaviour
+{
+    public Camera left;
+    public Camera right;
+    public Transform middle;
+
+    public DepthOfField dof;
+    
+    [Space(10)]
+    
+    [Tooltip("x rotation offset for cameras to fight z-fighting")]
+    public float xOffset = .001f;
+
+    public float lerp = .1f;
+
+    public float dofRange = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit cast;
+        bool hit = Physics.Raycast(middle.position, middle.forward, out cast, right.farClipPlane);
+        float dist = cast.distance;
+
+        dof.gaussianStart.min = Mathf.Lerp(dof.gaussianStart.min, dist - dofRange, lerp);
+        dof.gaussianEnd.min = Mathf.Lerp(dof.gaussianEnd.min, dist + dofRange, lerp);
+
+        Quaternion leftQ;
+        Quaternion rightQ;
+        
+        if (hit)
+        {
+            rightQ = Quaternion.Euler(0, Mathf.Atan2(-right.transform.localPosition.x, dist)*Mathf.Rad2Deg, 0) * Quaternion.Euler(xOffset,0,0);
+            leftQ = Quaternion.Euler(0, Mathf.Atan2(-left.transform.localPosition.x, dist)*Mathf.Rad2Deg, 0) * Quaternion.Euler(-xOffset,0,0);
+            
+            Debug.DrawLine(middle.position, cast.point, Color.green);
+        }
+        else
+        {
+            rightQ = Quaternion.Euler(xOffset, 0, 0);
+            leftQ = Quaternion.Euler(-xOffset, 0, 0);
+            
+            Debug.DrawLine(middle.position, middle.position + middle.forward*right.farClipPlane, Color.red);
+        }
+        
+        left.transform.localRotation = Quaternion.Slerp(left.transform.localRotation,leftQ, lerp);
+        right.transform.localRotation = Quaternion.Slerp(right.transform.localRotation, rightQ, lerp);
+        
+        Debug.DrawLine(left.transform.position, left.transform.position + left.transform.forward*left.farClipPlane);
+        Debug.DrawLine(right.transform.position, right.transform.position + right.transform.forward*right.farClipPlane);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject player;
+    public Rigidbody rB;
+
+    [Header("Settings")]
+    
+    public float walkSpeed = 100f;
+
+    public float mouseSens = 100f;
+
+    private float xRot = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSens * Time.deltaTime;
+        Vector2 walk = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * walkSpeed * Time.deltaTime;
+
+        xRot = Mathf.Clamp(xRot - mouse.y*25, -90f, 90f);
+        
+        camera.transform.localRotation = Quaternion.Euler(xRot, 0,0);
+        player.transform.RotateAround(Vector3.up, mouse.x);
+
+        rB.AddForce(player.transform.forward * walk.y * walkSpeed * Time.deltaTime +
+                    player.transform.right * walk.x * walkSpeed * Time.deltaTime,
+                    ForceMode.Force);
+    }
+}
